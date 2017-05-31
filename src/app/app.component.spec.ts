86e30e22b0f8fc07daac3e35912a1cfc517ab74d
cmoseses <4850090@qq.com>
2017-05-31 22:43:18 +0800

    [misc] modify test code for sub component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,12 +2,14 @@
 
 import {TestBed, async} from '@angular/core/testing';
 import {AppComponent} from './app.component';
+import {DynamicFormComponent} from './dynamic-form/dynamic-form.component';
 
 describe('AppComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        DynamicFormComponent
       ],
     });
     TestBed.compileComponents();
