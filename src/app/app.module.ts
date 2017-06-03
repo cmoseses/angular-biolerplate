a888d27a02f8947a6ba2fa35529dd3681f9edfed
cmoseses <4850090@qq.com>
2017-06-03 14:11:41 +0800

    [misc] delete useless code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,7 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {HttpModule} from '@angular/http';
 
 import {AppComponent} from './app.component';
-import {DynamicFormModule} from './dynamic-form/dynamic-form.module';
-import {DynamicFormQuestionModule} from './dynamic-form/dynamic-form-question/dynamic-form-question.module';
 import {AppRoutingModule} from './app-routing.module';
 import {LoginModule} from './login/login.module';
 import {HomeModule} from './home/home.module';
@@ -16,11 +12,6 @@ import {HomeModule} from './home/home.module';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule,
-    DynamicFormModule,
-    DynamicFormQuestionModule,
-    ReactiveFormsModule,
     LoginModule,
     HomeModule,
     AppRoutingModule
