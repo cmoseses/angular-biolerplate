6d871773bfd7bef660fe4b89940699b6566a60f1
cmoseses <4850090@qq.com>
2017-06-04 18:17:54 +0800

    [feature] import gird to home-content

--- src/app/home-content/home-content.module.ts
+++ src/app/home-content/home-content.module.ts
@@ -2,9 +2,10 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 
 import {HomeContentComponent} from './home-content.component';
+import {GirdModule} from '../gird/gird.module';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, GirdModule],
   declarations: [HomeContentComponent],
   exports: [HomeContentComponent]
 })
